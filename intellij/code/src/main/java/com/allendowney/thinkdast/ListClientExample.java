30c0b6844c507d79c017661fc010b52766cca8a5
JY <pyeonjy97@gmail.com>
2018-07-18 02:48:11 +0900

    prac 1 ~ p. 24

--- intellij/code/src/main/java/com/allendowney/thinkdast/ListClientExample.java
+++ intellij/code/src/main/java/com/allendowney/thinkdast/ListClientExample.java
@@ -1,25 +1,31 @@
 package com.allendowney.thinkdast;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListClientExample {
 	@SuppressWarnings("rawtypes")
 	private List list;
+	// overspecified
+//	private ArrayList list;
 
 	@SuppressWarnings("rawtypes")
 	public ListClientExample() {
-		list = new LinkedList();
+	    // interface-based programming
+        /**
+         * 특정 구체적인 클래스에 의존하지 않음.
+         * 추후 구현이 변경되도 인터페이스 그대로 이용 **/
+	    list = new ArrayList();
 	}
 
 	@SuppressWarnings("rawtypes")
 	public List getList() {
 		return list;
 	}
-
+	
 	public static void main(String[] args) {
 		ListClientExample lce = new ListClientExample();
-		@SuppressWarnings("rawtypes")
+        @SuppressWarnings("rawtypes")
 		List list = lce.getList();
 		System.out.println(list);
 	}
